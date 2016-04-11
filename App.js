ae40119ae7e88e33d4b140483b3a5611d0aa4b56
William Kammersell <william.kammersell@ca.com>
2016-04-11 12:03:20 -0600

    Add text preview.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ Ext.define('CustomApp', {
 		type: 'vbox',
 		align: 'left'
 	},
-	launch: function() {
+	launch: function() {	
 		// other blog options (that don't work yet)
 		// var blog_feed_url = "http://www.ca.com/us/rss/news/feed.aspx";
 		// var blog_feed_url = "https://medium.com/feed/ca-rabu-life";
@@ -15,6 +15,7 @@ Ext.define('CustomApp', {
 		var blog_feed_url = "https://www.rallydev.com/blog/all-blogs.xml";
 		var blog_feed;
 		var xmlhttp;
+		var max_content_length = 500;
 		
 		// load blog feed
 		// code for IE7+, Firefox, Chrome, Opera, Safari, SeaMonkey
@@ -41,7 +42,8 @@ Ext.define('CustomApp', {
 		for ( var i = 0; i < items.length; i++ ) {
 			// add the title as a header
 			var title = items[i].getElementsByTagName( "title" )[0].childNodes[0].nodeValue;
-			output_html += "<h3>" + title + "</h3>";
+			var post_link = items[i].getElementsByTagName( "link" )[0].childNodes[0].nodeValue;
+			output_html += "<h3><a href='" + post_link + "' target='_blank'>" + title + "</a></h3>";
 			
 			// add the author and date
 			var author = items[i].getElementsByTagName( "creator" )[0].childNodes[0].nodeValue;
@@ -50,6 +52,21 @@ Ext.define('CustomApp', {
 			var date_string = items[i].getElementsByTagName( "pubDate" )[0].childNodes[0].nodeValue;
 			output_html += "by " + "<a href='" + author_link + "' target='_blank' >" + author + "</a>";
 			output_html += " on " + new Date(date_string).toLocaleDateString();
+			output_html += "<br/>";
+			
+			// add a preview of the text
+			var content_html = items[i].getElementsByTagName( "description" )[0].childNodes[0].nodeValue;
+			// create a copy of the html without image src to avoid loading all images
+			var content_html_no_images = content_html.replace( /src=".*?"/g, 'src=""' );
+			alert( content_html_no_images );
+			var div = document.createElement("div");
+			div.innerHTML = content_html_no_images;
+			var content_text = div.textContent || div.innerText || "";
+			output_html += content_text.slice( 0, max_content_length );
+			// add an elipsis if the post content would go longer
+			if ( content_text.length > max_content_length ) {
+				output_html += "...";
+			}
 			
 			// add a separator
 			output_html += "<hr/>";
